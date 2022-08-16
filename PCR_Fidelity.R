21a00f844e82b38cfbf68858d83861928aca33e7
wilfriedgrange <110093777+wilfriedgrange@users.noreply.github.com>
2022-08-16 16:08:22 +0100

    Update PCR_Fidelity.R

--- PCR_Fidelity.R
+++ PCR_Fidelity.R
@@ -1,24 +1,28 @@
 #PCR Fidelity Calculator (R Code)
 # Adapted from doi.org/10.3929/ethz-a-006088024
-# W. Grange (2018)
+# W. Grange (2018/22)
+rm(list=ls()) # empty memory
+###################################
 #Declare Variables
+###################################
 # Rate
 u<-1.5E-4
 # Fragment length
-l<-2000
+l<-1000
 # Number of Cycles
-numcycles<-5
+numcycles<-25
+###################################
+#Fixed 
+###################################
 # Number of mutations to calculate
 mut<-10
 # Number of molecules
 N<-1000
 #Efficiency (doi.org/10.1093/nar/25.15.3082)
 lambda<-c(rep(0.872,20),rep(0.743,5),rep(0.146,5))
-#Init
-data<-data.frame(matrix(0,ncol = mut , nrow = (numcycles+1)))
-vec_p<-rep(0,mut)
-vec_p[1]<-N
-data[1,]<-vec_p
+###################################
+#Functions
+###################################
 #Shift Function (https://stackoverflow.com/questions/26997586/r-shifting-a-vector)
 shift <- function(x, n, invert=FALSE, default=0){
   stopifnot(length(x)>=n)
@@ -28,6 +32,13 @@ shift <- function(x, n, invert=FALSE, default=0){
   n <- ifelse(invert, n*(-1), n)
   return(c(rep(default, n), x[seq_len(length(x)-n)]))
 }
+###################################
+#Init
+###################################
+data<-data.frame(matrix(0,ncol = mut , nrow = (numcycles+1)))
+vec_p<-rep(0,mut)
+vec_p[1]<-N
+data[1,]<-vec_p
 #Calculate mutation prob
 pr_mutation<-sapply(1:mut, function(i){dmultinom(c((i-1),l-i+1),l,prob=c(u,1-u))})
 #First cycle (could be in the loop too)
@@ -48,9 +59,10 @@ if (sum(vec) != n){
 vec<-vec+vec_p
 molecules<-vec
 data[2,]<-molecules
-#Cycles
-for (k in 1:(numcycles-1))
-{
+###################################
+#Main
+###################################
+results<-sapply(1:(numcycles-1), function(k){
   # molecules has "mut" elements (molecules have 0, 1, 2 ... mutations)
   # Calculate number of new molecules generated
   n<-round(sum(molecules)*lambda[k+1])
@@ -79,8 +91,12 @@ for (k in 1:(numcycles-1))
   }
   SUM<-apply(Error_shifted,2,sum)
   # Update Molecules	
-  molecules<-molecules+SUM
+  molecules<<-molecules+SUM
   data[(k+2),]<-molecules
 }
+)
+###################################
+#output
+###################################
 res<-signif(100*molecules/sum(molecules),3)
 paste("[Code still under test] % molecules with 0 error:", res[1], ', 1 error: ', res[2], ', 2 errors: ', res[3])
